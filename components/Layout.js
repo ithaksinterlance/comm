beeb508491550ee7cae995af18009600d2393fac
ithaksinterlance <it.haksinterlance@gmail.com>
2022-11-05 17:38:04 +0400

    commit

---
+++ components/Layout.js
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Store } from "../utils/Store";
+
+function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " --HaksComm" : "HaksComm"}</title>
+        <meta name="description" content="Ecommerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link href="/" className="text-lg font-bold">
+              HaksComm
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+                {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          <p>Copyright @ 2022</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
